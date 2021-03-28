41e48748a04bcd21e2699344069037212658182f
dhock21 <54248237+dhock21@users.noreply.github.com>
2021-03-27 22:39:03 -0400

    Update dbhandler.php

--- includes/dbhandler.php
+++ includes/dbhandler.php
@@ -1,8 +1,8 @@
 <?php
 
 $servename = "localhost";
-$DBuname = "root"; //root
-$DBPass = "";
+$DBuname = "phpmyadmin"; //root
+$DBPass = "cs230lab";
 $DBname = "cs230";
 
 $conn = mysqli_connect($servename, $DBuname, $DBPass, $DBname);
